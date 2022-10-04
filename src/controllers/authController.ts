c404d2fab4c5ab00e1b9fb62214d62adb0535cd9
JV-Albuquerque-S <alb.albqrq@gmail.com>
2022-10-04 10:44:07 -0300

    feat: initial authorization controller

---
+++ src/controllers/authController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+//import { CreateUserData } from "../repositories/userRepositories";
+//import * as authService from "../services/authService"
+
+export async function signIn(req: Request, res: Response) {
+    //const user:CreateUserData = req.body;
+    //const token = await authService.signIn(user);
+
+    //res.send({token});
+}
+
+export async function signUp(req: Request, res: Response) {
+    //const {email, password}:CreateUserData = req.body;
+    //await authService.signUp({email, password});
+    res.sendStatus(201);
+}
